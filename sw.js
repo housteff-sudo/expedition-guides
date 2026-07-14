008933de76fef9761c0389ddda6b0adde886d247
housteff-sudo <houstef.f@gmail.com>
2026-07-14 11:32:19 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='eg-v1';
-const ASSETS=['./','./index.html','./assets/css/main.css','./assets/js/main.js','./rome/index.html','./rome/chapter-01/index.html'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
